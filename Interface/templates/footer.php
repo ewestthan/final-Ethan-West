1261447967ae587e066987960d7aa0583e895ee6
Ethan Patrick West <ethan.west@uvm.edu>
2022-04-25 11:46:10 -0400

    Finished creating structure

--- Interface/templates/footer.php
+++ Interface/templates/footer.php
@@ -1,5 +1,6 @@
 <footer>
-    
+    <p><a href="../index.php">Site Map</a></p>
+    <p><a href="//ewest3.w3.uvm.edu/cs148/Final-Ethan-West">Ethan West</a></p>
 </footer>
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Group Member:
 Ethan West
+
+Link to Final Site:
+https://ewest3.w3.uvm.edu/cs148/live-Final-Ethan-West/Interface/templates/index.php
